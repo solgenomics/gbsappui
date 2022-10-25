e029e1a5d682f3a2effef7c77a79c876c3138790
Amber Lockrow <awl67@cornell.edu>
2022-10-25 17:13:31 -0400

    remove extraneous line in entrypoint file, edit slurm configuration

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,10 @@ cp /gbsappui/slurm.conf  /etc/slurm/
 sed -i s/localhost/$HOSTNAME/g /etc/slurm/slurm.conf
 cp /gbsappui/config.sh  /GBSapp/examples/proj/
 /etc/init.d/munge start
-sacctmgr add cluster e954439d9ba1
 /etc/init.d/slurmctld start
 /etc/init.d/slurmd start
 touch /var/log/gbsappui/error.log
+#chmod 777 /var/log/gbsappui/error.log
 sleep 5
 /gbsappui/script/gbsappui_server.pl -r -p 8080 2> /var/log/gbsappui/error.log
 tail -f /var/log/gbsappui/error.log
