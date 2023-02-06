b3fe27f4890232208e7bde47fa9ac9d196ba2c97
Amber Lockrow <awl67@cornell.edu>
2023-02-06 15:13:07 -0500

    Reset entrypoint to sleep 5 and get username info from sgn

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,6 @@ cp /gbsappui/config.sh  /GBSapp/examples/proj/
 /etc/init.d/slurmd start
 touch /var/log/gbsappui/error.log
 #chmod 777 /var/log/gbsappui/error.log
-sleep 10
+sleep 5
 /gbsappui/script/gbsappui_server.pl -r -p 8090 2> /var/log/gbsappui/error.log
 tail -f /var/log/gbsappui/error.log
