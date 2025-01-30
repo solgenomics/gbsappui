62ea7652215658eed845dfe1d840200f4a5d2ba3
alockrow <awl67@cornell.edu>
2025-01-30 12:17:15 -0500

    Edit GBSapp scripts for manual ngm installationwith miniconda in a different location

---
+++ GBSapp_internal_parameters.sh
@@ -0,0 +1,142 @@
+
+red=$'\e[1;31m'
+green=$'\e[1;32m'
+yellow=$'\e[1;33m'
+blue=$'\e[1;34m'
+magenta=$'\e[1;35m'
+cyan=$'\e[1;36m'
+white=$'\e[0m'
+
+######################################################################################################################################################
+export GBSapp_dir=${GBSapp_dir%/*}
+export projdir=${projdir%/*}
+
+pop=${projdir%/}
+export pop=${pop##*/}
+
+cluster="$(grep cluster config.sh)"
+export cluster=${cluster//*=}
+
+######################################################################################################################################################
+# Software defined parameters
+
+export slurm_module=$((module --version) 2>&1 | head -n1)
+
+if [[ "$slurm_module" =~ "Module" ]]; then
+	module unload python
+	module add python
+	pythonversion=$((python --version) 2>&1)
+	if [[ "$pythonversion" =~ "Python 2" ]] || [[ "$pythonversion" =~ "Python 3" ]]; then
+		echo -e "${white}\n- Using $pythonversion\n ${white}"
+	fi
+	module unload R 2> /dev/null
+  module add R 2> /dev/null
+	module add r 2> /dev/null
+  Rversion=$((R --version) 2>&1)
+  if [[ "$Rversion" =~ "R version" ]]; then
+    echo -e "${white}\n- Using $Rversion\n ${white}"
+  fi
+fi
+
+Rversion=$((R --version) 2>&1)
+if [[ "$Rversion" =~ "R version" ]]; then
+  echo -e "${white}\n- Using $Rversion\n ${white}"
+else
+  echo -e "${white}- install R before proceeding ${white}"
+  echo -e "${white}- dependencies for R in linux: <sudo apt install libcurl4-openssl-dev> and <sudo apt install libssl-dev>"
+fi
+
+pythonversion=$((python --version) 2>&1)
+if [[ "$pythonversion" =~ "Python 2" ]]; then
+  echo -e "${white}\n- Using $pythonversion\n ${white}"
+else
+	echo -e "${white}- install python before proceeding ${white}"
+fi
+
+javaversion=$((update-alternatives --list java | grep 'java-8-openjdk') 2>&1)
+mkdir -p ~/bin
+PATH=~/bin:$PATH
+rm ~/bin/java
+ln -s $javaversion ~/bin/java
+javaversion=$((java -version) 2>&1)
+if [[ "$javaversion" =~ "1.8" ]]; then
+	echo -e "${white}\n- Using $javaversion\n ${white}"
+else
+	mkdir -p ~/bin
+	PATH=~/bin:$PATH
+	rm ~/bin/java
+	ln -s ${GBSapp_dir}/tools/jdk8*/bin/java ~/bin/java
+	javaversion=$((java -version) 2>&1)
+	if [[ "$javaversion" =~ "1.8" ]]; then
+		echo -e "${white}\n- Using $javaversion\n ${white}"
+	else
+		echo -e "${white}- install java version build 1.8 before proceeding ${white}"
+	fi
+fi
+mkdir -p ~/bin
+PATH=~/bin:$PATH
+rm ~/bin/java
+ln -s ${GBSapp_dir}/tools/jdk8*/bin/java ~/bin/java
+
+
+
+
+######################################################################################################################################################
+# tools
+export ngm=/root/miniconda3/bin/ngm
+export samtools=${GBSapp_dir}/tools/samtools*/samtools && samtools=${samtools//'//'/'/'}
+export bcftools=${GBSapp_dir}/tools/bcftools*/bcftools && bcftools=${bcftools//'//'/'/'}
+export bedtools=${GBSapp_dir}/tools/bedtools2/bin/bedtools && bedtools=${bedtools//'//'/'/'}
+export picard=${GBSapp_dir}/tools/picard.jar && picard=${picard//'//'/'/'}
+export GATK=${GBSapp_dir}/tools/gatk-4.2.6.1/gatk && GATK=${GATK//'//'/'/'}
+export java=${GBSapp_dir}/tools/jdk8*/bin/java && java=${java//'//'/'/'}
+export consambig=${GBSapp_dir}/tools/EMBOSS-6.6.0/emboss/consambig
+export mafft=${GBSapp_dir}/tools/mafft/mafft
+if command -v pigz &>/dev/null; then
+  export gzip=pigz
+else
+  export gzip=gzip
+fi
+
+samtoolsout=$($samtools --version | head -n 3)
+if [ -z "$samtoolsout" ];then
+  echo -e "${white}- samtools installation within GBSapp is probably missing a dependency on host system ${white}"
+  echo -e "${white}- GBSapp will use host system samtools installation ${white}"
+  if [[ "$cluster" == true ]]; then
+    module add samtools
+  fi
+  export samtools=samtools
+  $samtools --version | head -n 3
+else
+  $samtools --version | head -n 3
+fi
+
+bcftoolsout=$($bcftools --version | head -n 3)
+if [ -z "$bcftoolsout" ];then
+  echo -e "${white}- bcftools installation within GBSapp is probably missing a dependency on host system ${white}"
+  echo -e "${white}- GBSapp will use host system bcftools installation ${white}"
+  if [[ "$cluster" == true ]]; then
+    module add bcftools
+  fi
+  export bcftools=bcftools
+  $bcftools --version | head -n 3
+else
+  $bcftools --version | head -n 3
+fi
+
+bedtoolsout=$($bedtools --version | head -n 3)
+if [ -z "$bedtoolsout" ];then
+  echo -e "${white}- bedtools installation within GBSapp is probably missing a dependency on host system ${white}"
+  echo -e "${white}- GBSapp will use host system bcftools installation ${white}"
+  if [[ "$cluster" == true ]]; then
+    module add bedtools
+  fi
+  export bedtools=bedtools
+  $bedtools --version | head -n 3
+else
+  $bedtools --version | head -n 3
+fi
+
+cd $GBSapp_dir/tools/R
+Rscript ../../scripts/R/check_R_tools.R "${GBSapp_dir}/tools/R"
+cd $projdir
---
+++ install.sh
@@ -0,0 +1,254 @@
+#!/bin/bash
+
+red=$'\e[1;31m'
+green=$'\e[1;32m'
+yellow=$'\e[1;33m'
+blue=$'\e[1;34m'
+magenta=$'\e[1;35m'
+cyan=$'\e[1;36m'
+white=$'\e[0m'
+
+tools_dir=$(pwd)
+
+main2 () {
+  echo -e "${blue}\n############################################## ${yellow}\n- downloading and installing bcftools ${blue}\n##############################################${white}"
+  wget https://github.com/samtools/bcftools/releases/download/1.17/bcftools-1.17.tar.bz2 &&
+  tar -xvf bcftools-1.17.tar.bz2;  cd bcftools-1.17; make
+  rm ../bcftools-1.17.tar.bz2
+  cd $tools_dir
+}
+dirtool=bcftools-1.17
+if [ -d $dirtool ]; then
+  :
+else
+  echo -e "${magenta}- Performing installation of dependency (bcftools) ${white}"
+  main2 &>> ./log.out
+  if [ ! -d $dirtool ]; then
+      echo -e "${magenta} bcftools did not install properly ${white}"
+  fi
+fi
+
+
+main3 () {
+  echo -e "${blue}\n############################################## ${yellow}\n- downloading and installing bedtools ${blue}\n##############################################${white}"
+  wget https://github.com/arq5x/bedtools2/releases/download/v2.29.1/bedtools-2.29.1.tar.gz &&
+  tar -zxvf bedtools-2.29.1.tar.gz; cd bedtools2; make
+  rm ../bedtools-2.29.1.tar.gz
+  cd $tools_dir
+}
+dirtool=bedtools*
+if [ -d $dirtool ]; then
+  :
+else
+  echo -e "${magenta}- Performing installation of dependency (bedtools) ${white}"
+  main3 &>> ./log.out
+  if [ ! -d $dirtool ]; then
+      echo -e "${magenta} bedtools did not install properly ${white}"
+  fi
+fi
+
+
+main4 () {
+  echo -e "${blue}\n############################################## ${yellow}\n- downloading and installing samtools ${blue}\n##############################################${white}"
+  wget https://github.com/samtools/samtools/releases/download/1.17/samtools-1.17.tar.bz2 &&
+  tar -xvf samtools-1.17.tar.bz2;  cd samtools-1.17; make
+  rm ../samtools-1.17.tar.bz2
+  cd $tools_dir
+}
+dirtool=samtools*
+if [ -d $dirtool ]; then
+  :
+else
+  echo -e "${magenta}- Performing installation of dependency (samtools) ${white}"
+  main4 &>> ./log.out
+  if [ ! -d $dirtool ]; then
+      echo -e "${magenta} samtools did not install properly ${white}"
+  fi
+fi
+
+
+main5 () {
+  echo -e "${blue}\n############################################## ${yellow}\n- downloading PICARD tools ${blue}\n##############################################${white}"
+  wget https://github.com/broadinstitute/picard/releases/download/2.25.6/picard.jar
+  cd $tools_dir
+}
+dirtool=picard*
+if [ -f $dirtool ]; then
+  :
+else
+  echo -e "${magenta}- Performing installation of dependency (picard tools) ${white}"
+  main5 &>> ./log.out
+  if [ ! -f $dirtool ]; then
+      echo -e "${magenta} PICARD tools did not install properly ${white}"
+  fi
+fi
+
+
+main () {
+  echo -e "${blue}\n############################################## \n- installiing java 1.8. ${blue}\n##############################################${white}"
+  wget https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u322-b06/OpenJDK8U-jdk_x64_linux_hotspot_8u322b06.tar.gz
+  tar -xvf OpenJDK8U-jdk_x64_linux_hotspot_8u322b06.tar.gz; rm *tar.gz
+  cd $tools_dir
+}
+dirtool=*jdk*
+if [ -d $dirtool ]; then
+  :
+else
+  echo -e "${magenta}- Performing installation of dependency (java 1.8)${white}"
+  main &>> ./log.out
+  if [ ! -d $dirtool ]; then
+      echo -e "${magenta} java did not install properly ${white}"
+  fi
+fi
+
+main6 () {
+  echo -e "${blue}\n############################################## \n- installiing Emboss ${blue}\n##############################################${white}"
+  wget -m 'ftp://emboss.open-bio.org/pub/EMBOSS/'
+  mv emboss.open-bio.org/pub/EMBOSS/EMBOSS-6.6.0.tar.gz ./
+  rm -rf emboss.open-bio.org/
+  gunzip EMBOSS-6.6.0.tar.gz
+  tar xvf EMBOSS-6.6.0.tar
+  cd EMBOSS-6.6.0/
+  ./configure --without-x
+  make
+  cd ../
+  rm EMBOSS-6.6.0.tar
+  cd $tools_dir
+}
+dirtool=EMBOSS*
+if [ -d $dirtool ]; then
+  :
+else
+  echo -e "${magenta}- Performing installation of dependency (EMBOSS)${white}"
+  main6 &>> ./log.out
+  if [ ! -d $dirtool ]; then
+      echo -e "${magenta} Emboss did not install properly ${white}"
+  fi
+fi
+
+
+main7 () {
+  echo -e "${blue}\n############################################## \n- installing mafft ${blue}\n##############################################${white}"
+  wget https://mafft.cbrc.jp/alignment/software/mafft-7.505-without-extensions-src.tgz
+  tar zxvf mafft-7.505-without-extensions-src.tgz
+  rm mafft-7.505-without-extensions-src.tgz
+  cd mafft-7.505-without-extensions/core/
+  awk 'NR!=3{print $0}' Makefile | awk 'NR>1{print $0}' | cat <(printf "BINDIR = ${GBSapp_dir}tools/mafft\n") - | \
+  cat <(printf "PREFIX = ${GBSapp_dir}tools/mafft\n") - > makefile.tmp
+  mv makefile.tmp Makefile
+  make clean
+  make
+  make install
+  cd $tools_dir
+}
+dirtool=mafft-7.505-without-extensions
+if [ -d $dirtool ]; then
+  :
+else
+  echo -e "${magenta}- Performing installation of dependency (mafft)${white}"
+  main7 &>> ./log.out
+  if [ ! -d $dirtool ]; then
+      echo -e "${magenta} mafft did not install properly ${white}"
+  fi
+fi
+
+main8 () {
+  echo -e "${green}\n############################################## \n- downloading GATK \n##############################################${white}"
+  wget -O GATK4.2.6.1.zip "https://github.com/broadinstitute/gatk/releases/download/4.2.6.1/gatk-4.2.6.1.zip"
+  unzip GATK4.2.6.1.zip
+  rm GATK4.2.6.1.zip
+  cd $tools_dir
+}
+dirtool=gatk*
+if [ -d $dirtool ]; then
+  :
+else
+  echo -e "${magenta}- Performing installation of dependency (GATK) ${white}"
+  main8 &>> ./log.out
+  if [ ! -d $dirtool ]; then
+      echo -e "${magenta} GATK did not install properly ${white}"
+  fi
+fi
+
+
+main0 () {
+  if which R; then
+    :
+  else
+    module add R
+    if which R; then
+      :
+    else
+      echo -e "${magenta}- install R before proceeding ${white}" > /dev/tty
+      echo -e "${magenta}- dependencies for R in linux: <sudo apt install libcurl4-openssl-dev> and <sudo apt install libssl-dev> ${white}" > /dev/tty
+      exit 1
+    fi
+  fi
+}
+echo -e "${white}\n############################################## ${orange}\n- check for R installation ${white}\n##############################################${white}" &>> ./log.out
+main0 &>> ./log.out
+
+
+dirtool=R
+if [ -d $dirtool ]; then
+  cd $dirtool
+  R_dir=$(pwd)
+else
+  mkdir ./$dirtool
+  cd $dirtool
+  R_dir=$(pwd)
+fi
+
+
+main9 () {
+  echo -e "${blue}\n############################################## \n- installing R-package: reshape2  ${blue}\n##############################################${white}"
+  R -e 'install.packages("reshape2", dependencies = TRUE, lib="./")'
+  cd $R_dir
+}
+dirtool=./reshape2
+if [ -d $dirtool ]; then
+  :
+else
+  echo -e "${magenta}- Performing installation of R-package: reshape2 ${white}"
+  main9 &>> ./log.out
+  cd $R_dir
+  if [ ! -d $dirtool ]; then
+      echo -e "${magenta} R-package: reshape2 did not install properly ${white}"
+  fi
+fi
+
+
+main10 () {
+  echo -e "${blue}\n############################################## \n- installing R-package: ggplot2  ${blue}\n##############################################${white}"
+  R -e 'install.packages("ggplot2", dependencies = TRUE, repos="http://cran.r-project.org", lib="./")'
+  cd ../
+}
+dirtool=./ggplot2
+if [ -d $dirtool ]; then
+  :
+else
+  echo -e "${magenta}- Performing installation of R-package: ggplot2 ${white}"
+  main10 &>> ./log.out
+  cd $R_dir
+  if [ ! -d $dirtool ]; then
+      echo -e "${magenta} R-package: ggplot2 did not install properly ${white}"
+  fi
+fi
+
+
+main11 () {
+  echo -e "${blue}\n############################################## \n- installing R-package: CMplot  ${blue}\n##############################################${white}"
+  R -e 'install.packages("CMplot", dependencies = TRUE, repos="http://cran.r-project.org", lib="./")'
+  cd ../
+}
+dirtool=./CMplot
+if [ -d $dirtool ]; then
+  :
+else
+  echo -e "${magenta}- Performing installation of R-package: CMplot ${white}"
+  main11 &>> ./log.out
+  cd $R_dir
+  if [ ! -d $dirtool ]; then
+      echo -e "${magenta} R-package: CMplot did not install properly ${white}"
+  fi
+fi
